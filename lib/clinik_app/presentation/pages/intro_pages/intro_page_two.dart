842468fbd35aea3d389e040d903a895da0f22a67
teewhydot <tchipsical@gmail.com>
2023-03-12 09:32:05 +0100

    Debugging ui code for smaller screen size.

--- lib/clinik_app/presentation/pages/intro_pages/intro_page_two.dart
+++ lib/clinik_app/presentation/pages/intro_pages/intro_page_two.dart
@@ -11,80 +11,80 @@ class IntroPageTwo extends StatelessWidget {
     double fem = MediaQuery.of(context).size.width / baseWidth;
     return Scaffold(
         body: SafeArea(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Column(
-            children: [
-              Center(
-                child: Container(
-                  margin:
-                      EdgeInsets.fromLTRB(0 * fem, 0 * fem, 0 * fem, 90 * fem),
-                  child: Image.asset(
-                    Assets.imagesLogoMedicine143,
-                    height: 33 * fem,
-                    width: 33 * fem,
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                Center(
+                  child: Container(
+                    margin: EdgeInsets.fromLTRB(
+                        0 * fem, 0 * fem, 0 * fem, 90 * fem),
+                    child: Image.asset(
+                      Assets.imagesLogoMedicine143,
+                      height: 33 * fem,
+                      width: 33 * fem,
+                    ),
                   ),
                 ),
-              ),
-              Container(
-                margin:
-                    EdgeInsets.fromLTRB(0.5 * fem, 0 * fem, 0 * fem, 13 * fem),
-                child: Text(
-                  'Let\'s get acquainted',
-                  textAlign: TextAlign.center,
-                  style: kHeaderTextStyle,
+                Container(
+                  margin: EdgeInsets.fromLTRB(
+                      0.5 * fem, 0 * fem, 0 * fem, 13 * fem),
+                  child: Text(
+                    'Let\'s get acquainted',
+                    textAlign: TextAlign.center,
+                    style: kHeaderTextStyle,
+                  ),
                 ),
-              ),
-              Container(
-                margin:
-                    EdgeInsets.fromLTRB(0.5 * fem, 0 * fem, 0 * fem, 72 * fem),
-                constraints: BoxConstraints(
-                  maxWidth: 316 * fem,
+                Container(
+                  margin: EdgeInsets.fromLTRB(
+                      0.5 * fem, 0 * fem, 0 * fem, 72 * fem),
+                  constraints: BoxConstraints(
+                    maxWidth: 316 * fem,
+                  ),
+                  child: Text(
+                    'Pellentesque placerat arcu in risus facilisis, sed laoreet eros laoreet.',
+                    textAlign: TextAlign.center,
+                    style: kSubHeaderTextStyle,
+                  ),
                 ),
-                child: Text(
-                  'Pellentesque placerat arcu in risus facilisis, sed laoreet eros laoreet.',
-                  textAlign: TextAlign.center,
-                  style: kSubHeaderTextStyle,
-                ),
-              ),
-              Container(
-                margin:
-                    EdgeInsets.fromLTRB(0.5 * fem, 0 * fem, 0 * fem, 18 * fem),
-                padding:
-                    EdgeInsets.fromLTRB(18 * fem, 18 * fem, 15 * fem, 8 * fem),
-                width: double.infinity,
-                height: 119 * fem,
-                decoration: BoxDecoration(
-                  color: const Color(0xfff4f6f5),
-                  borderRadius: BorderRadius.circular(18 * fem),
-                ),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Container(
-                      height: 63 * fem,
-                      width: 63 * fem,
-                      decoration: BoxDecoration(
-                        color: kAccentPrimaryGreen,
-                        borderRadius: BorderRadius.circular(13 * fem),
-                      ),
-                      child: Center(
-                        child: Image.asset(
-                          Assets.imagesIconPatient,
-                          width: 26.7 * fem,
-                          height: 33 * fem,
+                Container(
+                  margin: EdgeInsets.fromLTRB(
+                      0.5 * fem, 0 * fem, 0 * fem, 18 * fem),
+                  padding: EdgeInsets.fromLTRB(
+                      18 * fem, 18 * fem, 15 * fem, 8 * fem),
+                  width: double.infinity,
+                  height: 119 * fem,
+                  decoration: BoxDecoration(
+                    color: const Color(0xfff4f6f5),
+                    borderRadius: BorderRadius.circular(18 * fem),
+                  ),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        height: 63 * fem,
+                        width: 63 * fem,
+                        decoration: BoxDecoration(
+                          color: kAccentPrimaryGreen,
+                          borderRadius: BorderRadius.circular(13 * fem),
+                        ),
+                        child: Center(
+                          child: Image.asset(
+                            Assets.imagesIconPatient,
+                            width: 26.7 * fem,
+                            height: 33 * fem,
+                          ),
                         ),
                       ),
-                    ),
-                    addHorizontalSpacing(20),
-                    SizedBox(
-                      width: 200 * fem,
-                      child: Column(
+                      addHorizontalSpacing(20),
+                      Column(
                         mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           SizedBox(
-                            width: double.infinity,
+                            width: 200,
                             child: Text(
                               'I’m a patient',
                               textAlign: TextAlign.left,
@@ -98,47 +98,45 @@ class IntroPageTwo extends StatelessWidget {
                           ),
                         ],
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-              Container(
-                margin:
-                    EdgeInsets.fromLTRB(0.5 * fem, 0 * fem, 0 * fem, 18 * fem),
-                padding:
-                    EdgeInsets.fromLTRB(18 * fem, 18 * fem, 15 * fem, 8 * fem),
-                width: double.infinity,
-                height: 119 * fem,
-                decoration: BoxDecoration(
-                  color: const Color(0xfff4f6f5),
-                  borderRadius: BorderRadius.circular(18 * fem),
-                ),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Container(
-                      height: 63 * fem,
-                      width: 63 * fem,
-                      decoration: BoxDecoration(
-                        color: kDarkGreyShade400,
-                        borderRadius: BorderRadius.circular(13 * fem),
-                      ),
-                      child: Center(
-                        child: Image.asset(
-                          Assets.imagesIconDoctor,
-                          width: 26.7 * fem,
-                          height: 33 * fem,
+                Container(
+                  margin: EdgeInsets.fromLTRB(
+                      0.5 * fem, 0 * fem, 0 * fem, 18 * fem),
+                  padding: EdgeInsets.fromLTRB(
+                      18 * fem, 18 * fem, 15 * fem, 8 * fem),
+                  width: double.infinity,
+                  height: 119 * fem,
+                  decoration: BoxDecoration(
+                    color: const Color(0xfff4f6f5),
+                    borderRadius: BorderRadius.circular(18 * fem),
+                  ),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        height: 63 * fem,
+                        width: 63 * fem,
+                        decoration: BoxDecoration(
+                          color: kDarkGreyShade400,
+                          borderRadius: BorderRadius.circular(13 * fem),
+                        ),
+                        child: Center(
+                          child: Image.asset(
+                            Assets.imagesIconDoctor,
+                            width: 26.7 * fem,
+                            height: 33 * fem,
+                          ),
                         ),
                       ),
-                    ),
-                    addHorizontalSpacing(20),
-                    SizedBox(
-                      width: 200 * fem,
-                      child: Column(
+                      addHorizontalSpacing(20),
+                      Column(
                         mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           SizedBox(
-                            width: double.infinity,
+                            width: 200,
                             child: Text(
                               'I’m a Doctor',
                               textAlign: TextAlign.left,
@@ -152,20 +150,22 @@ class IntroPageTwo extends StatelessWidget {
                           ),
                         ],
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            ],
-          ),
-          Column(
-            children: [
-              ButtonWidget(
-                  fem: fem, color: kAccentPrimaryGreen, buttonText: 'Continue'),
-              addVerticalSpacing(30)
-            ],
-          ),
-        ],
+              ],
+            ),
+            Column(
+              children: [
+                ButtonWidget(
+                    fem: fem,
+                    color: kAccentPrimaryGreen,
+                    buttonText: 'Continue'),
+                addVerticalSpacing(30)
+              ],
+            ),
+          ],
+        ),
       ),
     ));
   }
