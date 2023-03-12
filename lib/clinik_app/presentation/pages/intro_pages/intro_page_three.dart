849d9119e3b2e7a4f0cce1f1dc05f6735cfb6806
teewhydot <tchipsical@gmail.com>
2023-03-12 09:49:42 +0100

    Debugging ui code for smaller screen size.

--- lib/clinik_app/presentation/pages/intro_pages/intro_page_three.dart
+++ lib/clinik_app/presentation/pages/intro_pages/intro_page_three.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:medical_clinik/clinik_app/presentation/widgets/constants/constants.dart';
 import 'package:medical_clinik/generated/assets.dart';
 
@@ -11,59 +12,60 @@ class IntroPageThree extends StatelessWidget {
     return Scaffold(
         body: SingleChildScrollView(
       child: SafeArea(
-        child: Column(
-          children: [
-            Center(
-              child: Container(
-                margin:
-                    EdgeInsets.fromLTRB(0 * fem, 0 * fem, 0 * fem, 90 * fem),
-                child: Image.asset(
-                  Assets.imagesLogoMedicine143,
-                  height: 33 * fem,
-                  width: 33 * fem,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Center(
+                child: Container(
+                  margin:
+                      EdgeInsets.fromLTRB(0 * fem, 0 * fem, 0 * fem, 90 * fem),
+                  child: Image.asset(
+                    Assets.imagesLogoMedicine143,
+                    height: 33 * fem,
+                    width: 33 * fem,
+                  ),
                 ),
               ),
-            ),
-            Container(
-              margin:
-                  EdgeInsets.fromLTRB(0.5 * fem, 0 * fem, 0 * fem, 13 * fem),
-              child: Text(
-                'A little about yourself',
-                textAlign: TextAlign.center,
-                style: kHeaderTextStyle,
+              Container(
+                margin:
+                    EdgeInsets.fromLTRB(0.5 * fem, 0 * fem, 0 * fem, 13 * fem),
+                child: Text(
+                  'A little about yourself',
+                  textAlign: TextAlign.center,
+                  style: kHeaderTextStyle,
+                ),
               ),
-            ),
-            Container(
-              margin:
-                  EdgeInsets.fromLTRB(0.5 * fem, 0 * fem, 0 * fem, 72 * fem),
-              constraints: BoxConstraints(
-                maxWidth: 316 * fem,
+              Container(
+                margin:
+                    EdgeInsets.fromLTRB(0.5 * fem, 0 * fem, 0 * fem, 72 * fem),
+                constraints: BoxConstraints(
+                  maxWidth: 316 * fem,
+                ),
+                child: Text(
+                  'Pellentesque placerat arcu in risus facilisis, sed laoreet eros laoreet.',
+                  textAlign: TextAlign.center,
+                  style: kSubHeaderTextStyle,
+                ),
               ),
-              child: Text(
-                'Pellentesque placerat arcu in risus facilisis, sed laoreet eros laoreet.',
-                textAlign: TextAlign.center,
-                style: kSubHeaderTextStyle,
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Your gender',
+                  style: kHeaderTextStyle,
+                ),
               ),
-            ),
-            Container(
-              margin:
-                  EdgeInsets.fromLTRB(0 * fem, 0 * fem, 250 * fem, 13 * fem),
-              child: Text(
-                'Your gender',
-                style: kHeaderTextStyle,
-              ),
-            ),
-            Container(
-              margin:
-                  EdgeInsets.fromLTRB(0.5 * fem, 0 * fem, 0 * fem, 42 * fem),
-              width: double.infinity,
-              height: 99 * fem,
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Expanded(
-                    child: Container(
-                      // maleKF6 (9:443)
+              addVerticalSpacing(20),
+              Container(
+                margin:
+                    EdgeInsets.fromLTRB(0.5 * fem, 0 * fem, 0 * fem, 42 * fem),
+                width: double.infinity,
+                height: 99 * fem,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
                       margin: EdgeInsets.fromLTRB(
                           0 * fem, 0 * fem, 10 * fem, 0 * fem),
                       padding: EdgeInsets.fromLTRB(
@@ -104,9 +106,7 @@ class IntroPageThree extends StatelessWidget {
                         ],
                       ),
                     ),
-                  ),
-                  Expanded(
-                    child: Container(
+                    Container(
                       // maleKF6 (9:443)
                       margin: EdgeInsets.fromLTRB(
                           0 * fem, 0 * fem, 10 * fem, 0 * fem),
@@ -148,121 +148,94 @@ class IntroPageThree extends StatelessWidget {
                         ],
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            Container(
-              margin:
-                  EdgeInsets.fromLTRB(0 * fem, 0 * fem, 240 * fem, 13 * fem),
-              child: Text(
-                'Your birthday',
-                style: kHeaderTextStyle,
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Your gender',
+                  style: kHeaderTextStyle,
+                ),
               ),
-            ),
-            Container(
-              // birthdayWkp (9:421)
-              margin:
-                  EdgeInsets.fromLTRB(0 * fem, 0 * fem, 92.5 * fem, 38 * fem),
-              height: 85 * fem,
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    width: 79 * fem,
-                    height: double.infinity,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                          // dayCNk (9:423)
-                          margin: EdgeInsets.fromLTRB(
-                              0 * fem, 0 * fem, 0 * fem, 7 * fem),
-                          child: Text(
-                            'Day:',
-                            style: kSubHeaderTextStyle,
-                          ),
-                        ),
-                        TextFormField(
-                          maxLength: 2,
-                          // day (9:424)
-                          decoration: InputDecoration(
-                            counterText: '',
-                            contentPadding: EdgeInsets.fromLTRB(
-                                5 * fem, 4 * fem, 5 * fem, 3 * fem),
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10 * fem),
-                            ),
-                          ),
-                        ),
-                      ],
+              Container(
+                margin:
+                    EdgeInsets.fromLTRB(0 * fem, 0 * fem, 0 * fem, 38 * fem),
+                height: 100 * fem,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    BirthdayFieldWidget(
+                      fem: fem,
+                      width: 70,
+                      label: 'Day',
                     ),
-                  ),
-                  addHorizontalSpacing(16),
-                  SizedBox(
-                    width: 79 * fem,
-                    height: double.infinity,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                          // mounthJKJ (9:427)
-                          margin: EdgeInsets.fromLTRB(
-                              0 * fem, 0 * fem, 0 * fem, 7 * fem),
-                          child: Text('Mounth:', style: kSubHeaderTextStyle),
-                        ),
-                        TextFormField(
-                          maxLength: 2,
-                          // day (9:424)
-                          decoration: InputDecoration(
-                            counterText: '',
-                            contentPadding: EdgeInsets.fromLTRB(
-                                5 * fem, 4 * fem, 5 * fem, 3 * fem),
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10 * fem),
-                            ),
-                          ),
-                        ),
-                      ],
+                    addHorizontalSpacing(16),
+                    BirthdayFieldWidget(
+                      fem: fem,
+                      width: 79,
+                      label: 'Month',
                     ),
-                  ),
-                  addHorizontalSpacing(16),
-                  SizedBox(
-                    width: 79 * fem,
-                    height: double.infinity,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                          // year2uW (9:431)
-                          margin: EdgeInsets.fromLTRB(
-                              0 * fem, 0 * fem, 0 * fem, 7 * fem),
-                          child: Text(
-                            'Year:',
-                            style: kSubHeaderTextStyle,
-                          ),
-                        ),
-                        TextFormField(
-                          maxLength: 4,
-                          // day (9:424)
-                          decoration: InputDecoration(
-                            counterText: '',
-                            contentPadding: EdgeInsets.fromLTRB(
-                                5 * fem, 4 * fem, 5 * fem, 3 * fem),
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10 * fem),
-                            ),
-                          ),
-                        ),
-                      ],
+                    addHorizontalSpacing(16),
+                    BirthdayFieldWidget(
+                      fem: fem,
+                      width: 99,
+                      label: 'Year',
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     ));
+  }
+}
+
+class BirthdayFieldWidget extends StatelessWidget {
+  const BirthdayFieldWidget({
+    super.key,
+    required this.fem,
+    required this.width,
+    required this.label,
+  });
+
+  final double fem;
+  final double width;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: width * fem,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            // dayCNk (9:423)
+            margin: EdgeInsets.fromLTRB(0 * fem, 0 * fem, 0 * fem, 7 * fem),
+            child: Text(
+              label,
+              style: kSubHeaderTextStyle,
+            ),
+          ),
+          TextFormField(
+            maxLength: 2,
+            // day (9:424)
+            decoration: InputDecoration(
+              counterText: '',
+              contentPadding:
+                  EdgeInsets.fromLTRB(5 * fem, 4 * fem, 5 * fem, 3 * fem),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10 * fem),
+              ),
+            ),
+
+            maxLengthEnforcement: MaxLengthEnforcement.enforced,
+          ),
+        ],
+      ),
+    );
   }
 }
